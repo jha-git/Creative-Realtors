5cb08f12e275880ea4f821ddf7ca1211d121a2f3
Workspace <gobind.jha@yahoo.com>
2020-04-24 10:30:06 +0530

    Updates

---
+++ precache-manifest.a783df0961b24a74d11214596f079cdf.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e4134da3c914b0c88c399aeddc527e",
+    "url": "/Web-Duck/index.html"
+  },
+  {
+    "revision": "ef67ad029c6b17144bda",
+    "url": "/Web-Duck/static/css/main.070f6e92.chunk.css"
+  },
+  {
+    "revision": "a349317f1df5dd6b78b5",
+    "url": "/Web-Duck/static/js/2.a12ff866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Duck/static/js/2.a12ff866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef67ad029c6b17144bda",
+    "url": "/Web-Duck/static/js/main.ed249b3c.chunk.js"
+  },
+  {
+    "revision": "291821eb7ea8a72d9018",
+    "url": "/Web-Duck/static/js/runtime-main.7c274bd6.js"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/Web-Duck/static/media/back.f250e126.jpg"
+  },
+  {
+    "revision": "969e5be7437fec09b6fdf906ba75c672",
+    "url": "/Web-Duck/static/media/expand.969e5be7.svg"
+  },
+  {
+    "revision": "581fc6ad45a1428157168bfff7c40e03",
+    "url": "/Web-Duck/static/media/gal-1.581fc6ad.jpeg"
+  },
+  {
+    "revision": "9865e700f1cd5e17135a6f75da6b1378",
+    "url": "/Web-Duck/static/media/gal-10.9865e700.jpeg"
+  },
+  {
+    "revision": "5536a90f3836ebf8085ec0ae46984867",
+    "url": "/Web-Duck/static/media/gal-11.5536a90f.jpeg"
+  },
+  {
+    "revision": "83ee1d1e0e83198aa12b8a63af3f8a2b",
+    "url": "/Web-Duck/static/media/gal-12.83ee1d1e.jpeg"
+  },
+  {
+    "revision": "b4740c9c0d2cec5640904289867c8ce6",
+    "url": "/Web-Duck/static/media/gal-13.b4740c9c.jpeg"
+  },
+  {
+    "revision": "486c8880b61fee96a95de85288a1e6ae",
+    "url": "/Web-Duck/static/media/gal-14.486c8880.jpeg"
+  },
+  {
+    "revision": "ee7b3ca7ee80e0b5c62391c6ec93a671",
+    "url": "/Web-Duck/static/media/gal-2.ee7b3ca7.jpeg"
+  },
+  {
+    "revision": "628f87aeedf507d437f6aeb12bcc4215",
+    "url": "/Web-Duck/static/media/gal-3.628f87ae.jpeg"
+  },
+  {
+    "revision": "c6fff3a13ac92abd83a80c208c4dda53",
+    "url": "/Web-Duck/static/media/gal-4.c6fff3a1.jpeg"
+  },
+  {
+    "revision": "4114ded407c6764a037d322452fd6ca0",
+    "url": "/Web-Duck/static/media/gal-5.4114ded4.jpeg"
+  },
+  {
+    "revision": "aa2376b5a04a3bf548d9f1773f580da1",
+    "url": "/Web-Duck/static/media/gal-6.aa2376b5.jpeg"
+  },
+  {
+    "revision": "6085a127abe2dab815ab22ec0a9135d1",
+    "url": "/Web-Duck/static/media/gal-7.6085a127.jpeg"
+  },
+  {
+    "revision": "f30fbda69d5eca2f340eb6d4d093a914",
+    "url": "/Web-Duck/static/media/gal-8.f30fbda6.jpeg"
+  },
+  {
+    "revision": "92758fee44f610a3b225c33ff90a9333",
+    "url": "/Web-Duck/static/media/gal-9.92758fee.jpeg"
+  },
+  {
+    "revision": "693ed88f35e5f0dd882067b4ca5de257",
+    "url": "/Web-Duck/static/media/global.693ed88f.svg"
+  },
+  {
+    "revision": "c649a1df3d155f6c1e9b1385ef0f1f13",
+    "url": "/Web-Duck/static/media/heart.c649a1df.svg"
+  },
+  {
+    "revision": "d11b666fa6c694c620ff867c6669cf7f",
+    "url": "/Web-Duck/static/media/hero.d11b666f.jpeg"
+  },
+  {
+    "revision": "5120ad222b9080b343187d3c51e851f3",
+    "url": "/Web-Duck/static/media/house-1.5120ad22.jpeg"
+  },
+  {
+    "revision": "ed12ff11bf26663331ad01ff18d3ebfb",
+    "url": "/Web-Duck/static/media/house-2.ed12ff11.jpeg"
+  },
+  {
+    "revision": "521c065d9e8bc6ff2ec850a3ea58a4bf",
+    "url": "/Web-Duck/static/media/house-3.521c065d.jpeg"
+  },
+  {
+    "revision": "83ee1d1e0e83198aa12b8a63af3f8a2b",
+    "url": "/Web-Duck/static/media/house-4.83ee1d1e.jpeg"
+  },
+  {
+    "revision": "721effb4a9ef7f3ac5e281cb13cda239",
+    "url": "/Web-Duck/static/media/house-5.721effb4.jpeg"
+  },
+  {
+    "revision": "ee021f8ac6ebfadeab35a43e6ef6b306",
+    "url": "/Web-Duck/static/media/house-6.ee021f8a.jpeg"
+  },
+  {
+    "revision": "d7d0212d8bc3c46656875534372e6e95",
+    "url": "/Web-Duck/static/media/key.d7d0212d.svg"
+  },
+  {
+    "revision": "d7acd8abab0ddd74035300209f741e18",
+    "url": "/Web-Duck/static/media/location.d7acd8ab.svg"
+  },
+  {
+    "revision": "f0745b3e9828a5d3b7a5be54b761a87f",
+    "url": "/Web-Duck/static/media/lock.f0745b3e.svg"
+  },
+  {
+    "revision": "ee16c7b8a84ac078afc2b46923cc6187",
+    "url": "/Web-Duck/static/media/logo-bi.ee16c7b8.png"
+  },
+  {
+    "revision": "e5c174a02df208c513cfb932428e84c9",
+    "url": "/Web-Duck/static/media/logo-forbes.e5c174a0.png"
+  },
+  {
+    "revision": "a473e3878ca044d42ba86f99b685ba47",
+    "url": "/Web-Duck/static/media/logo-techcrunch.a473e387.png"
+  },
+  {
+    "revision": "163647972af7c274d250a57afd3d1aa5",
+    "url": "/Web-Duck/static/media/logo.16364797.png"
+  },
+  {
+    "revision": "e57c90127e930cc95709958676db4f27",
+    "url": "/Web-Duck/static/media/presentation.e57c9012.svg"
+  },
+  {
+    "revision": "406400ead91831b741f5246c50146142",
+    "url": "/Web-Duck/static/media/realtor-1.406400ea.jpeg"
+  },
+  {
+    "revision": "c11e84dcdad4dbf1cb49ff5813ed6611",
+    "url": "/Web-Duck/static/media/realtor-2.c11e84dc.jpeg"
+  },
+  {
+    "revision": "af9ff289ef4aa6e35a09b66ec1e42419",
+    "url": "/Web-Duck/static/media/realtor-3.af9ff289.jpeg"
+  },
+  {
+    "revision": "24268b24588bddc750dc3b45bd820459",
+    "url": "/Web-Duck/static/media/story-1.24268b24.jpeg"
+  },
+  {
+    "revision": "915f2bd1ebf80f9d775aa9954f294269",
+    "url": "/Web-Duck/static/media/story-2.915f2bd1.jpeg"
+  },
+  {
+    "revision": "b1cd5c3196828b5c26b74295902c7eb9",
+    "url": "/Web-Duck/static/media/trophy.b1cd5c31.svg"
+  },
+  {
+    "revision": "0cb49ca546fb58356376f1c47a03e649",
+    "url": "/Web-Duck/static/media/user.0cb49ca5.svg"
+  }
+]);
